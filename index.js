73688e88e6ca73257a86c590bdded6085283a4dc
wang <114349746+wang-on-top@users.noreply.github.com>
2023-11-24 13:17:18 -0500

    v1

---
+++ index.js
@@ -0,0 +1,48 @@
+let iModal = {
+    constants: {
+        container: "presentation",
+        headerClass: "css-19hcsz9-Typography enj526p0",
+        descriptionClass: "css-1ql0wvf-Typography enj526p0",
+        errorRoute: "/student/error/modal",
+        dashRoute: "/student/dashboard/home",
+        buttonId: "continue-button-button",
+        dashId : "StudentDashboard-g38",
+        get dashHook() {
+            return Object.values(document.getElementById(this.dashId))[1].children[0]._owner.stateNode;
+        },
+    },
+    cachedGoto: null, // Save the goto function
+    closeModal: function () {
+        iModal.goto(iModal.constants.dashRoute);
+    },
+    goto: function (route) {
+        return this.cachedGoto(route);
+    },
+    showModal: function (config) {
+        // Save the original goto function before it's unavailable
+        iModal.cachedGoto = iModal.constants.dashHook.props.navigationToPageByRelativeUrl;
+
+        function handleMutation(mutationsList, observer) {
+            for (const mutation of mutationsList) {
+                if (mutation.type === 'childList') {
+                    mutation.addedNodes.forEach(node => {
+                        if (node.role === iModal.constants.container && document.getElementById(iModal.constants.dashId) === null) {
+                            let header = document.getElementsByClassName(iModal.constants.headerClass)[0];
+                            let desc = document.getElementsByClassName(iModal.constants.descriptionClass)[0];
+                            let button = document.getElementById(iModal.constants.buttonId);
+
+                            // Check if useInnerHTML is true, then use innerHTML, else use textContent
+                            header[config.useInnerHTML ? 'innerHTML' : 'textContent'] = config.title;
+                            desc[config.useInnerHTML ? 'innerHTML' : 'textContent'] = config.description;
+                            button.onclick = config.callback;
+                        }
+                    });
+                }
+            }
+        }
+
+        const observer = new MutationObserver(handleMutation);
+        observer.observe(document, { childList: true, subtree: true });
+        iModal.goto(iModal.constants.errorRoute);
+    },
+};
